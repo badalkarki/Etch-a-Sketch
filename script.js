8829925d36f0aa0554731bd3d777b39d5705b046
Badal karki <128877662+badalkarki@users.noreply.github.com>
2026-02-03 19:17:12 +0545

    Creates 16 x 16 grid
    Use Js DOM to create a skeleton of Div's and adds CSS to make it look
    like grid, but just with display Flex.

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("or Morning");
+const parentDiv = document.querySelector(".container");
+for(let i = 0; i< 256; i++) {
+    const childDiv = document.createElement("div");
+    childDiv.classList.add("child");
+    parentDiv.appendChild(childDiv);
+}
+
+
+
+
+
+
+
