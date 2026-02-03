4788f7b55c2e58db8991d96516f80b4241de07e7
Badal karki <128877662+badalkarki@users.noreply.github.com>
2026-02-03 22:56:20 +0545

    Create Js code to fire event and change background color when mouse is hovered
    Also corrected the bug: previously the grid was 15 x 16, increased the loop
    by 16 more elements and also changed the :nth-child(...) part to 17n+1;
    Which ensures that the column is also equal.

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const parentDiv = document.querySelector(".container");
-for(let i = 0; i< 256; i++) {
+for(let i = 0; i < 256+16; i++) {
     const childDiv = document.createElement("div");
     childDiv.classList.add("child");
     parentDiv.appendChild(childDiv);
 }
 
-
-
-
-
+const childDiv = document.querySelectorAll(".child");
+childDiv.forEach(div => {
+    div.addEventListener("mouseover", () => {
+        div.style.setProperty("background-color"," rgb(180,255,180)")
+    })
+}); 
 
 
