09b0ecfa693d1c32a3a86fa87e689a1296135bc4
Badal karki <128877662+badalkarki@users.noreply.github.com>
2026-02-04 15:27:23 +0545

    Update Js—— Create a dynamic button to change grid
    Kept every thing inside a function for to reduce redundancy of the code
    and keep it clean. Adds logic for removing gird and also altering it.
    
    Updates CSS so— on every sizes of grid the width and height adjust itself.

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
-const parentDiv = document.querySelector(".container");
-for(let i = 0; i < 256+16; i++) {
-    const childDiv = document.createElement("div");
-    childDiv.classList.add("child");
-    parentDiv.appendChild(childDiv);
+function setChildDiv (gridSize) {
+    const parentDiv = document.querySelector(".container");
+    for(let i = 0; i < (gridSize * gridSize) + gridSize; i++) {
+        const childDiv = document.createElement("div");
+        childDiv.classList.add("child");
+        parentDiv.appendChild(childDiv);
+    }
 }
 
-const childDiv = document.querySelectorAll(".child");
-childDiv.forEach(div => {
-    div.addEventListener("mouseover", () => {
-        div.style.setProperty("background-color"," rgb(180,255,180)")
+function setInkTrailStyle() {
+    const childDiv = document.querySelectorAll(".child");
+    childDiv.forEach(div => {
+        div.addEventListener("mouseover", () => {
+            div.style.setProperty("background-color"," rgb(180,255,180)")
+         })
+    }) 
+}
+
+
+const btn = document.querySelector("button");
+let gridSize;
+btn.addEventListener("click",() => {
+    gridSize = +prompt("Enter number betwn 4-100 to change grid size");
+    console.log(gridSize);       
+    alterGridSize(gridSize);
+    setInkTrailStyle();
+})
+
+
+function alterGridSize (gridSize) {
+    removePreviousGrid(); 
+    if (gridSize >=4 && gridSize <= 100) { 
+        setChildDiv(gridSize);
+    }else{
+    alert("Enter number between the range");
+    }
+}
+
+function removePreviousGrid () {
+    const childDiv = document.querySelectorAll(".child");
+    childDiv.forEach(div => {
+        div.remove()
     })
-}); 
+}
 
-
+  setChildDiv(28);
+  setInkTrailStyle();
