edf544440c40b51cdb615ef0083744c62c7ab3fc
Badal karki <128877662+badalkarki@users.noreply.github.com>
2026-02-04 22:30:22 +0545

    Nest more div inside childRowsDiv to act like col
    Create a function called setChildColsDiv to nest the rows with more divs
    to make it act like a grid and also add some css property.

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-function setChildRowsDiv (gridSize) {
+function setChildRowsDiv () {
     const parentDiv = document.querySelector(".container");
-    for(let i = 0; i < (gridSize * gridSize) + gridSize; i++) {
+    for(let i = 0; i < 8; i++) {
         const childRowsDiv = document.createElement("div");
         childRowsDiv.classList.add("childRows");
         parentDiv.appendChild(childRowsDiv);
     }
 }
 
-// function setchildColsDiv(){
-//     const document.querySelectorAll(".")
-// }
+function setChildColsDiv(){
+    const childRowsDiv = document.querySelectorAll(".childRows");
+    console.log(childRowsDiv);
+    childRowsDiv.forEach(rows => {
+        for(let i = 0; i < 8; i++){
+             const childColsDiv = document.createElement("div");
+             childColsDiv.classList.add("childCols");
+             rows.appendChild(childColsDiv);
+        }
+    })
+}
+
 
 function setInkTrailStyle() {
     const childDiv = document.querySelectorAll(".child");
@@ -46,6 +55,6 @@ function removePreviousGrid () {
         div.remove()
     })
 }
-
-  setChildRowsDiv(28);
+  setChildRowsDiv();
+  setChildColsDiv();
   setInkTrailStyle();
